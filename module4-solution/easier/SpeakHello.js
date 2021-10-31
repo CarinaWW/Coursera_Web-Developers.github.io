ce49568df80853d01f124f9b752ed37163960fbe
吴Y <1043377859@qq.com>
2021-10-31 23:22:42 +0800

    'module4-solution'

--- module4-solution/easier/SpeakHello.js
+++ module4-solution/easier/SpeakHello.js
@@ -15,8 +15,8 @@ var speakWord = "Hello";
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-helloSpeaker.speak = function (){
-  console.log(speakWord + " " + helloSpeaker);
+helloSpeaker.speak = function (names){
+  console.log(speakWord + " " + names);
 };
 // function speak(name) {
 //   console.log(speakWord + " " + name);
